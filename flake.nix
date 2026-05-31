f6f2864002d8179a9e3f06fdcca11c8eeefab15e
Jeong, Hun <blmarket@gmail.com>
2026-05-30 22:30:49 -0700

    Change nixos

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A minimal entity framework";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     crane.url = "github:ipetkov/crane";
   };
